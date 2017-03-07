68e2ca5a31cb55d1b24a277144b26d0badd2c6d4
Pedro Castanheira <josepedrocastanheira@gmail.com>
2017-03-07 11:33:50 +0000

    Print in reverse order
    Changed in the printing order. The first item to be printed is now the
    recent and the last is the oldest.

--- lib/printer.rb
+++ lib/printer.rb
@@ -7,7 +7,7 @@ class Printer
           "credit".center(8) + " || " +
           "debit".center(8) + " || " +
           "balance".center(8) + "\n"
-    transactions_list.each do |transaction|
+    transactions_list.reverse.each do |transaction|
       print "#{transaction[0].strftime("%d/%m/%Y")}".center(12) + " || " +
             "#{transaction[2] if transaction[1]=='deposit'}".rjust(8) + " || " +
             "#{transaction[2] if transaction[1]=='withdrawal'}".rjust(8) + " || " +
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -11,15 +11,15 @@ describe Printer do
                 "debit".center(8) + " || " +
                 "balance".center(8) + "\n"
 
-      line1 = "#{transactions_list[0][0].strftime("%d/%m/%Y")}".center(12) + " || " +
+      line1 = "#{transactions_list[1][0].strftime("%d/%m/%Y")}".center(12) + " || " +
+      "#{transactions_list[1][2] if transactions_list[1][1]=='deposit'}".rjust(8) + " || " +
+      "#{transactions_list[1][2] if transactions_list[1][1]=='withdrawal'}".rjust(8) + " || " +
+      "#{transactions_list[1][3]}".rjust(8) + "\n"
+
+      line2 = "#{transactions_list[0][0].strftime("%d/%m/%Y")}".center(12) + " || " +
               "#{transactions_list[0][2] if transactions_list[0][1]=='deposit'}".rjust(8) + " || " +
               "#{transactions_list[0][2] if transactions_list[0][1]=='withdrawal'}".rjust(8) + " || " +
               "#{transactions_list[0][3]}".rjust(8) + "\n"
-
-      line2 = "#{transactions_list[1][0].strftime("%d/%m/%Y")}".center(12) + " || " +
-              "#{transactions_list[1][2] if transactions_list[1][1]=='deposit'}".rjust(8) + " || " +
-              "#{transactions_list[1][2] if transactions_list[1][1]=='withdrawal'}".rjust(8) + " || " +
-              "#{transactions_list[1][3]}".rjust(8) + "\n"
 
       expect { Printer.statement(transactions_list) }.to output(header + line1 + line2).to_stdout
     end
